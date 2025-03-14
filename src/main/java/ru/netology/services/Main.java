a3019d8657834914ca613c6f46d0475da314692e
Rustam Korsovetskiy <rustamkorsovetskiy2013@gmail.com>
2025-03-14 23:04:31 +0300

    Vacacion double

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         VacationServices service = new VacationServices();
-        int vacaciones = service.calculate(100_000, 60_000, 150_000);
+        int vacaciones = service.calculate(10_000, 3_000, 20_000);
         System.out.println("Количество месяцев отпуска: " + vacaciones);
+
+        vacaciones = service.calculate(100_000, 60_000, 150_000);
+        System.out.println("Новое количество месяцев отпуска: " + vacaciones);
     }
 }
